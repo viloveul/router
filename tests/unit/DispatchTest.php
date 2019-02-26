a09c93b3f2a9dd9b1f2c551ca97b86436d8dbdb7
Fajrul Akbar Zuhdi <fajrulaz@gmail.com>
2019-02-26 16:15:01 +0700

    add simple tests

---
+++ tests/unit/DispatchTest.php
@@ -0,0 +1,56 @@
+<?php 
+
+class DispatchTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+    
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function testInstanceWithCollection()
+    {
+        $e = null;
+        try {
+            $collection = new Viloveul\Router\Collection();
+            $router = new Viloveul\Router\Dispatcher($collection);
+        } catch (Exception $e) {
+            // do nothing
+        }
+        $this->tester->assertNotInstanceOf(Exception::class, $e);
+    }
+
+    public function testCanDispatchRoute()
+    {
+        $foo = new Viloveul\Router\Route('GET /foo/{:bar}', function($bar) {
+            return $bar;
+        });
+        $collection = new Viloveul\Router\Collection();
+        $collection->add('foo', $foo);
+        $router = new Viloveul\Router\Dispatcher($collection);
+        $router->dispatch('GET', '/foo/dor');
+        $route = $router->routed();
+        $this->tester->assertEquals('foo', $route->getName());
+    }
+
+    public function testNotFound()
+    {
+        $this->tester->expectThrowable(Viloveul\Router\NotFoundException::class, function() {
+            $foo = new Viloveul\Router\Route('GET /foo/{:bar}', function($bar) {
+                return $bar;
+            });
+            $collection = new Viloveul\Router\Collection();
+            $collection->add('foo', $foo);
+            $router = new Viloveul\Router\Dispatcher($collection);
+            $router->dispatch('GET', '/foot/dor');
+        });
+    }
+}
